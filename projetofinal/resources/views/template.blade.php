f89e9dd5d9ac43a9ae5bd8284e2120183ecad619
Brenno Hoffmann <brenno_hoffmann@outlook.com>
2019-07-24 15:40:09 -0300

    logout finalizado

--- projetofinal/resources/views/template.blade.php
+++ projetofinal/resources/views/template.blade.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield ('title')</title>
+    <title>@yield('titulo')</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
 </head>
-<body>
 
 <body>
 
     <header>
         <nav class="navbar navbar-expand-lg  d-flex justify-content-between">
             <a href="/">
-                <img src="{{asset('Imagens/polvopi.png')}}" alt="logo" width="150px" height="75px">
+                <img src="{{asset('imagens/polvopi.png')}}" alt="logo" width="150px" height="75px">
             </a>
 
             <div id="cssmenu">
                 <ul>
                     
-                    <li><a href="outlet">Outlet</a></li>
-                    <li><a href="about">About</a></li>
-                    <li><a href="products">Products</a></li>
-                    <li><a href="/" id="homestyle"><i class="fa fa-home"></i>Home</a></li>
+                    <li><a href="custom">Default</a></li>
+                    <li><a href="about">Sobre</a></li>
+                    <li><a href="cadastrar">Cadastrar Produtos</a></li>
+                    <li><a href="/" id="homestyle"><i class="fa fa-home"></i> Home</a></li>
                 </ul>
             </div>
             <div class="d-flex carrinho justify-content-around">
@@ -163,15 +163,6 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
-
-
-
-
-
-
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
+
 </html>
