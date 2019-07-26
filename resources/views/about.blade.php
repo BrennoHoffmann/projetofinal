f3cf54fa60e2f421e3239cdaad3c594c58822b7a
Matheus Pavan <malimavan@gmail.com>
2019-07-26 15:54:37 -0300

    att front end about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,1 +1,12 @@
 @extends ('template')
+
+@section ('content')
+
+<div class="fundoabout d-flex justify-content-center">
+  <div class="container">
+    <h2 class="flipcardpalmeiras">O melhor e-comerce de roupas da internet!</h2>
+    <h4 class="lead">Escolha suas vestimentas conforme as suas necessidades, com a facilidade e a modernidade de um site
+    desenvolvido justamente para você!.</h4>
+  </div>
+</div>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,17 +2,6 @@
 
 @section('content')
 
-<ul class="slider">
-     <li>
-           <img src="images/1.jpg" />
-     </li>
-     <li>
-           <img src="images/2.jpg" />
-     </li>
-     <li>
-           <img src="images/3.jpg" />
-     </li>
- </ul>
 
 
 
